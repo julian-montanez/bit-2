19f4ca38ee0289b5d0279417a59b17bbe662037d
julian <juanmonrrou4@gmail.com>
2025-06-01 02:50:18 -0500

    otra comiteada

--- script/script.js
+++ script/script.js
@@ -2,10 +2,10 @@ const lista = document.querySelector("#estudiantes")
 
 let carta = ``
 
-fetch('file.json')
+fetch('json/file.json')
     .then((res) => res.json())
     .then((info) => {
-    //console.log(info[3].projects[1].score.length)
+        //console.log(info[3].projects[1].score.length)
         for (let i = 0; i < info.length; i++) {
             function promedio(nota){
                 let total = 0
@@ -22,24 +22,26 @@ fetch('file.json')
                     return nota
                 }
             }
-        carta += 
+            let horas = info[i].intensity
+            let horas1 = horas.replace("hours","")
+            carta += 
             `<div class="contenedor">
                 <div class="carta cara-delantera">
-                    <img src="${info[i].usernameGithub == false ? "assets/usuario-no-foto.png" : `https://github.com/${info[i].usernameGithub}.png`}" class="foto" alt="foto del estudiante">
+                    <img src="${info[i].usernameGithub == false ? "assets/usuario-no-foto.png" : `https://github.com/${info[i].usernameGithub}.png`}" class="foto" loading="lazy" alt="foto del estudiante">
                     <hr>
                     <p class="nombre">${info[i].student}</p>
                     <p class="code">N°${info[i].code}</p>
                 </div>
                 <div class="carta cara-trasera">
                     <h2>intencidad del curso</h2>
-                    <p>${info[i].intensity} horas</p>
+                    <p>${horas1} horas</p>
                     <h3>notas de proyectos</h3>
                     <ul>
                         <li>${info[i].projects[0].name}: <br>${promedio(info[i].projects[0].score)}</li>
                         <li>${info[i].projects[1].name}: <br>${Math.floor(promedio(info[i].projects[1].score))}</li>
                     </ul>
                     <div class="contenedor-boton">
-                        ${info[i].usernameGithub == false ? `<div class="aviso"><p>el estudiante no posee GitHub</p></div>` : `<a href="${`https://github.com/${info[i].usernameGithub}`}" class="git" target="blank"><button>link de <img class="git-logo" src="assets/git-logo.png" alt="git"></button></a>`}
+                        ${info[i].usernameGithub == false ? `<div class="aviso"><p>el estudiante no posee GitHub</p></div>` : `<a href="${`https://github.com/${info[i].usernameGithub}`}" class="git" id="git" target="blank"><p>Link de </p><img class="git-logo" src="assets/GitHub-Emblema.png" alt="git"></a>`}
                     </div>
                 </div>
             </div>`;
@@ -49,17 +51,3 @@ fetch('file.json')
     .catch((err) => {
     console.log('error:', err);
 });
-
-
-
-/*fetch('file.json')
-    .then((res) => res.json())
-    .then((info) => {
-    for (let i = 0; i < info.length; i++) {
-        console.log(info[i].student);
-    }
-    })
-    .catch((err) => {
-    console.log('error:', err);
-});*/
-
